06cb5ff308f4deeccb0d63d8a51686af573448bb
Phantom-VK <vikramadityakhupse@gmail.com>
2024-06-25 05:36:38 +0530

    Added functionality to ask user for permissions

--- app/src/main/java/com/example/locationapp/MainActivity.kt
+++ app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -1,28 +1,40 @@
 package com.example.locationapp
 
+import android.Manifest
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat
 import com.example.locationapp.ui.theme.LocationAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel: LocationViewModel by viewModels()
             LocationAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyApp(viewModel)
                 }
             }
         }
@@ -30,17 +42,71 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(viewModel: LocationViewModel){
+    val context = LocalContext.current
+    val locationUtils = LocationUtils(context)
+    LocationDisplay(context = context, viewModel = viewModel , locationUtils = locationUtils)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LocationAppTheme {
-        Greeting("Android")
+fun LocationDisplay(context: Context,viewModel:LocationViewModel, locationUtils: LocationUtils) {
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = { permissions ->
+            if (permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true
+                && permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+            ) {
+                //Have location access
+            } else {
+                // ASK FOR ACCESS
+                val rationalRequired = ActivityCompat.shouldShowRequestPermissionRationale(
+                    context as MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                if (rationalRequired) {
+                    Toast.makeText(
+                        context, "Location access is required for this feature to work!",
+                        Toast.LENGTH_LONG
+                    )
+                        .show()
+                }else{
+                    Toast.makeText(
+                        context, "Location access is required for this feature to work! Please enable it in mobile settings",
+                        Toast.LENGTH_LONG
+                    )
+                        .show()
+                }
+
+            }
+
+        })
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = "Location Not Available!")
+
+        Button(onClick = {
+            if (locationUtils.hasLocationPermission(context)) {
+                //Display location
+            } else {
+                // Ask for permission
+                requestPermissionLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                )
+            }
+        }) {
+            Text(text = "Get Location")
+
+        }
+
     }
 }
